24c2e911cf199a3d62c71f589b50d57b57f423f6
Bay Bui <baybui@ucla.edu>
2021-06-12 12:24:01 -0700

    updated ability to delete reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -89,7 +89,7 @@ const thoughtController = {
   removeReaction({ params }, res) {
     Thought.findOneAndUpdate(
       { _id: params.thoughtId },
-      { $pull: { reaction: { reactionId: params.reactionId } } },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
       { new: true }
     )
       .then((dbUserData) => res.json(dbUserData))
